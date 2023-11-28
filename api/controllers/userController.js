1cde79768a9385ce6002260ac3c74d64803a8aa7
Nico Gilardoni <nicogilardonik@gmail.com>
2023-11-28 18:55:16 -0300

    cambios en la respuesta

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -50,9 +50,13 @@ const controller = {
         return res.status(404).json({ message: 'User not found' });
       }
 
-      res.json(user);
+      //res.json(user);
+      res.status(200).json({
+        ok: true,
+        data: user,
+      });
     } catch (err) {
-      res.status(500).json({ message: err.message });
+      res.status(500).json({ message: err.message, ok: false });
     }
   }
   /*notifySupports: async (req, res) => {
