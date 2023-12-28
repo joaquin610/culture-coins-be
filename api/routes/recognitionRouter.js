1e13aeb3eaf939bde8c464326470f2f2dba764fa
joaquin610 <joaquinbanchero610@hotmail.com>
2023-12-28 02:19:02 -0300

    traer ultimas notificaiones

--- api/routes/recognitionRouter.js
+++ api/routes/recognitionRouter.js
@@ -4,6 +4,8 @@ const recognition = require('../controllers/recognitionController')
 
 //router.get('/:id', validateJWT, carts.detail)
 router.post('/', recognition.add)
+router.get('/lastMinute', recognition.listLastMinute);
 router.get('/:user', recognition.list);
 router.get('/', recognition.list);
+
 module.exports = router;
