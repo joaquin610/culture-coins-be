34991e43f036f70cad88218f73d979fccb7d285c
joaquin610 <joaquinbanchero610@hotmail.com>
2024-01-03 23:43:09 -0300

    Revert "traer ultimas notificaiones"
    This reverts commit 1e13aeb3eaf939bde8c464326470f2f2dba764fa.

--- api/routes/recognitionRouter.js
+++ api/routes/recognitionRouter.js
@@ -4,8 +4,6 @@ const recognition = require('../controllers/recognitionController')
 
 //router.get('/:id', validateJWT, carts.detail)
 router.post('/', recognition.add)
-router.get('/lastMinute', recognition.listLastMinute);
 router.get('/:user', recognition.list);
 router.get('/', recognition.list);
-
 module.exports = router;
